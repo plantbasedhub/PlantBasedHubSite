6874c8efe5a2c39d63f40d17cfcc6285c16ded69
ludvikcruz <lucas.2001.cruz@gmail.com>
2024-11-29 00:02:15 +0000

    Criação da secção About na LandingPage com redirecionamento necessário para a mesma. Reestruturação do Carousel.jsx e implementação do ficheiro .css dessa mesma secção.

--- plantbasedhub/src/pages/Home/index.tsx
+++ plantbasedhub/src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import Head from "next/head";
-import Carousel from "../../components/Carousel"; // Caminho relativo correto para Carousel.jsx
-import Navbar from "../../components/Navbar"; // Caminho relativo correto para Navbar.tsx
-import styles from "../../styles/home.module.css"; // Caminho relativo correto para home.module.css
+import Head from "next/head"; // Certifique-se de importar o Head
+import Navbar from "../../components/Navbar"; // Navbar principal
+import WhatIsPlantBasedHub from "../../components/WhatIsPlantBasedhub"; // Importando o componente
+import styles from "../../styles/home.module.css"; // CSS principal
+import Carousel from "../../components/Carousel";
 
 export default function Home() {
   return (
-    <div className={`${styles.page}`}>
-      {/* Metadata da Página */}
+    <div className={styles.page}>
+      {/* Metadata */}
       <Head>
         <title>PlantBased Hub - Vegan Worldwide App</title>
         <meta
@@ -20,7 +21,7 @@ export default function Home() {
       <Navbar />
 
       {/* Conteúdo Principal */}
-      <div className={`${styles.container}`}>
+      <div className={styles.container}>
         <header className="text-center py-8">
           <h1 className="text-5xl font-bold text-green-800 mb-4">
             PlantBased Hub
@@ -30,20 +31,23 @@ export default function Home() {
           </p>
         </header>
 
+        
+
+          {/* Integração do componente Carousel */}
         <main className="mt-5">
           <div className="container mx-auto px-4 text-center">
-            {/* Subtítulo */}
             <p className="text-lg text-green-700 mb-8">
               PlantBased Hub is more than just an app – it’s a thriving community
               for vegans and eco-enthusiasts. Share delicious recipes, connect
               with like-minded individuals, shop sustainably, and inspire change.
               Together, we create a better future.
             </p>
-
-            {/* Carrossel */}
             <Carousel />
           </div>
         </main>
+
+          {/* Integração do componente WhatIsPlantBasedHub */}
+          <WhatIsPlantBasedHub />
       </div>
     </div>
   );
