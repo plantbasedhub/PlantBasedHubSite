65401d4defb3e0a3906d506aec80db52a063fd52
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2025-02-27 14:46:26 +0000

    BugFix

--- src/pages/api/auth/register.js
+++ src/pages/api/auth/register.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import prisma from '../../../lib/prisma';
+import prisma from '@/lib/prisma';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
     });
     res.status(201).json({ user });
   } catch (error) {
+    console.error('Error registering user:', error); // Agora está sendo usado no console
     res.status(500).json({ error: 'Error registering user' });
   }
 }
