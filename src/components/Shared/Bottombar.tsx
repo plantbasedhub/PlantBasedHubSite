05c9a9eea5d57cf773574fe35ca522d69fb280e1
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2025-02-27 14:39:49 +0000

    bug fix

--- src/components/Shared/Bottombar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Bottombar = () => {
-  return (
-    <div>Bottombar</div>
-  )
-}
--- src/components/Shared/LeftSidebar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
