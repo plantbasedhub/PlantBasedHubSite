ab6af705b846518f7520139056ab8ddc9c5f0308
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2025-03-11 12:28:54 +0000

    landingpage with css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Bem-vindo ao Next.js com Pages Router!</h1>
-      <Link href="/Home/Auth/login">Ir para Login</Link>
-      <Link href="/Home/Auth/register">Ir para Registo</Link>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <Link href="#about">About us</Link>
+          <Link href="#contact">Contact us</Link>
+        </nav>
+        <h1 className={styles.logo}>
+          PlantBased <span>Hub</span>
+        </h1>
+        <div className={styles.authButtons}>
+          <button className={styles.login}>Login</button>
+          <button className={styles.register}>Register</button>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <h2>Good quality work at sensible prices</h2>
+        <div className={styles.buttons}>
+          <button className={styles.startProject}>Start your project</button>
+          <button className={styles.continueProject}>Continue your project</button>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="" alt="Construction Workers" width={300} height={200} />
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <div className={styles.footerLinks}>
+          <Link href="#privacy">Privacy policy</Link>
+          <Link href="#terms">Terms</Link>
+        </div>
+        <p>Copyright © 2025 PlantBasedHub All Rights Reserved.</p>
+      </footer>
     </div>
   );
 }
