686c5392d4bbe09b40539d45d3e66d419f72f25c
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2025-03-17 10:19:24 +0000

    Speed insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -11,7 +11,9 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        
         <body>
+        <SpeedInsights />
           <Main />
           <NextScript />
         </body>
